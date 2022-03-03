5a2d947d7b3ae49cbfbf665d510d7e0ccd368f8d
98tmurphy <53626316+98tmurphy@users.noreply.github.com>
2022-03-03 13:49:51 -0600

    readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # MednetCodingChallenge
+## Installation
+To run the program, first make sure nodejs is installed on the mnachine. Then simply clone and run server.js.
+
+## API Documentation
+To call the API, use the following format:
+
+
--- server.js
+++ server.js
@@ -19,9 +19,9 @@ app.get("/check", (req, res) => {
     */
     
     var inputUnit     = req.query.iu
-    var inputTemp     = parseFloat(req.query.it)
+    var inputTemp     =parseFloat(req.query.it)
     var targetUnit    = req.query.tu
-    var studentAnswer = parseFloat(req.query.sa)
+    var studentAnswer = Math.round(parseFloat(req.query.sa))
     var response = {grade: ''}
 
     //make sure the units are proper and the values are numbers
@@ -34,54 +34,54 @@ app.get("/check", (req, res) => {
    
     //celsius to fahrenheit
     else if(inputUnit == 'c' && targetUnit == 'f'){
-        response.grade = studentAnswer == tempConvert.celsiusToFahrenheit(inputTemp) ? 'Correct':'Incorrect'
+        response.grade = studentAnswer == Math.round(tempConvert.celsiusToFahrenheit(inputTemp)) ? 'Correct':'Incorrect'
     }
     //celsius to kelvin
     else if(inputUnit == 'c' && targetUnit == 'k'){
-        response.grade = studentAnswer == tempConvert.celsiusToKelvin(inputTemp) ? 'Correct':'Incorrect'
+        response.grade = studentAnswer == Math.round(tempConvert.celsiusToKelvin(inputTemp)) ? 'Correct':'Incorrect'
     }
     //celsius to rankine
     else if(inputUnit == 'c' && targetUnit == 'r'){
-        response.grade = studentAnswer == tempConvert.celsiusToRankine(inputTemp) ? 'Correct':'Incorrect'
+        response.grade = studentAnswer == Math.round(tempConvert.celsiusToRankine(inputTemp)) ? 'Correct':'Incorrect'
     }
 
     //farhrenheit to9 celsius
     else if(inputUnit == 'f' && targetUnit == 'c'){
-        response.grade = studentAnswer == tempConvert.fahrenheitToCelsius(inputTemp) ? 'Correct':'Incorrect'
+        response.grade = studentAnswer == Math.round(tempConvert.fahrenheitToCelsius(inputTemp)) ? 'Correct':'Incorrect'
     } 
     //fahrenheit to kelvin
     else if(inputUnit == 'f' && targetUnit == 'k'){
-        response.grade = studentAnswer == tempConvert.fahrenheitToKelvin(inputTemp) ? 'Correct':'Incorrect'
+        response.grade = studentAnswer == Math.round(tempConvert.fahrenheitToKelvin(inputTemp)) ? 'Correct':'Incorrect'
     }
     //fahrenheit to rankine
     else if(inputUnit == 'f' && targetUnit == 'r'){
-        response.grade = studentAnswer == tempConvert.fahrenheitToRankine(inputTemp) ? 'Correct':'Incorrect'
+        response.grade = studentAnswer == Math.round(tempConvert.fahrenheitToRankine(inputTemp)) ? 'Correct':'Incorrect'
     }
 
     //kelvin to celsius
     else if(inputUnit == 'k' && targetUnit == 'c'){
-        response.grade = studentAnswer == tempConvert.kelvinToCelsius(inputTemp) ? 'Correct':'Incorrect'
+        response.grade = studentAnswer == Math.round(tempConvert.kelvinToCelsius(inputTemp)) ? 'Correct':'Incorrect'
     }
     //kelvin to fahrenheit
     else if(inputUnit == 'k' && targetUnit == 'f'){
-        response.grade = studentAnswer == tempConvert.kelvinToFahrenheit(inputTemp) ? 'Correct':'Incorrect'
+        response.grade = studentAnswer == Math.round(tempConvert.kelvinToFahrenheit(inputTemp)) ? 'Correct':'Incorrect'
     }
     //kelvin to rankine
     else if(inputUnit == 'k' && targetUnit == 'r'){
-        response.grade = studentAnswer == tempConvert.kelvinToRankine(inputTemp) ? 'Correct':'Incorrect'
+        response.grade = studentAnswer == Math.round(tempConvert.kelvinToRankine(inputTemp)) ? 'Correct':'Incorrect'
     }
 
     //rankine to celsius
     else if(inputUnit == 'r' && targetUnit == 'c'){
-        response.grade = studentAnswer == tempConvert.rankineToCelsius(inputTemp) ? 'Correct':'Incorrect'
+        response.grade = studentAnswer == Math.round(tempConvert.rankineToCelsius(inputTemp)) ? 'Correct':'Incorrect'
     }
     //rankine to fahrenheit
     else if(inputUnit == 'r' && targetUnit == 'f'){
-        response.grade = studentAnswer == tempConvert.rankineToFahrenheit(inputTemp) ? 'Correct':'Incorrect'
+        response.grade = studentAnswer == Math.round(tempConvert.rankineToFahrenheit(inputTemp)) ? 'Correct':'Incorrect'
     }
     //rankine to kelvin
     else if(inputUnit == 'r' && targetUnit == 'k'){
-        response.grade = studentAnswer == tempConvert.rankineToKelvin(inputTemp) ? 'Correct':'Incorrect'
+        response.grade = studentAnswer == Math.round(tempConvert.rankineToKelvin(inputTemp)) ? 'Correct':'Incorrect'
     }
     res.status(200).send(response)
 })  
